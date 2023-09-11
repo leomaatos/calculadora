92f413a3161a33b6bac39e8d70f431175dc8e7a0
Leandro Matos <76454918+leomaatos@users.noreply.github.com>
2023-09-11 17:38:43 -0300

    Update mainapp.py

--- mainapp.py
+++ mainapp.py
@@ -22,3 +22,48 @@ class MainApp(App):
         ]
         for row in buttons:
             h_layout=BoxLayout()
+            for label in row:
+                button=Button(
+                    text=label,
+                    pos_hint={"center_x": 0.5, "center_y": 0.5},
+                )
+            button.bind(on_press=self.on_button_press)
+            h_layout.add_widget(button)
+        main_layout.add_widget(h_layout)
+
+        equals_button=Button(
+            text="=", pos_hint={"center_x": 0.5, "center_y": 0.5}
+        )
+        equals_button.bind(on_press=self.on_solution)
+        main_layout.add_widget(equals_button)
+
+        return main_layout
+    
+    def on_button_press(self, instance):
+        current=self.solution.text
+        button_text=instance.text
+
+        if button_text == "C":
+            # clear the solution widget
+            self.solution.text=""
+        else:
+            if current and (
+                self.last_was_operator and button_text in self.operators):
+                # Don’t add two operators right after each other
+                return
+            
+            elif current == "" and button_text in self.operators:
+                # First character cannot be an operator
+                return
+            
+            else:
+                new_text=current+button_text
+                self.solution.text=new_text
+            self.last_button=button_text
+            self.last_was_operator=self.last_button in self.operators
+
+    def on_solution(self,instance):
+        text=self.solution.text
+        if text:
+            solution=str(eval(self.solution.text))
+            self.solution.text=solution
